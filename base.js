940c7024b429af0cb3cda3953804a3d9ccee3645
CharlieMa <machaodev@gmail.com>
2016-07-04 18:05:28 +0800

    Refactoring of js

--- base.js
+++ base.js
@@ -1,23 +1,13 @@
 $( document ).ready(function() {
 
-    var tune = new Array("E","A","G","B","A","E");
-    var tuneIndex = new Array(28,33,38,43,47,52);
+    var tune = new Array("E","A","D","G","B","E");
+    var tunePosition = new Array(4,9,2,7,11,4);
 
     var majorPosition = new Array(0,2,4,5,7,9,11);
     var minorPosition = new Array(0,2,3,5,7,8,10);
 
-    var noteArr = new Array("C","#C","D","#D","E","F","#F","G","#G","A","#A","B");
-    var noteMap = new Array();
+    var twelveToneEqualTemperament = new Array("C","#C","D","#D","E","F","#F","G","#G","A","#A","B");
     
-    var j=0;
-    for(var i =0; i<82; i=i+1){
-        noteMap.push(noteArr[j]);
-        j=j+1;
-        if(j==12){
-            j=0;
-        }
-    }
-
     $("#selcet_key").change(function(){  
         var key = $("#selcet_key").val();  
         var scale = $("#selcet_scale").val();  
@@ -34,101 +24,89 @@ $( document ).ready(function() {
 
     });  
 
-    function changeFingerBoard(key,scale){
-        var offset = 0 ;
-        var note = new Array();
-        switch (key){
-            case "C":
-                offset = 0 ;
-                break;
-            case "D":
-                offset = 2 ;
-                break;
-            case "E":
-                offest = 4 ;
-                break;
-            case "F":
-                offest = 5 ;
-                break;
-            case "G":
-                offest = 7 ;
-                break;
-            case "A":
-                offest = 9 ;
-                break;
-            case "B":
-                offest = 11 ;
-                break;
-        }
-        var noteKeyArr = new Array();
-        for(var i=0;i<noteArr.length;i=i+1){
-            var indexOffset = 0 ;
-            if(i+offest<noteArr.length){
-                indexOffset=i+offest;
-            }else{
-                indexOffset = i+offest - noteArr.length;
-            }
-            noteKeyArr.push(noteArr[indexOffset]);
 
-        }
+    function getNotePosition(note){
+        return twelveToneEqualTemperament.indexOf(note);
+    
+    }
+    function getKeyNotes(key){
+        var keyNotes = new Array();
+    var offset = getNotePosition(key);
+        for(var i=0;i<twelveToneEqualTemperament.length;i=i+1 ){
+        keyNotes.push(twelveToneEqualTemperament[(i+offset)%twelveToneEqualTemperament.length ] );
+    }
+    return keyNotes;
+    }
+
+    function getScalePosition(scale){
         switch (scale){
-            case "major":
-                for(var i=0;i<majorPosition.length;i=i+1 ){
-                     note.push(noteKeyArr[majorPosition[i]] );
-                }
-                break ;
-            case "minor":
-                for(var i=0;i<minorPosition.length;i=i+1 ){
-                     note.push(noteKeyArr[minorPosition[i]]);
-                }
-                break;
-        }
+       case "major":
+          return majorPosition ;
+      case "minor":
+                return minorPosition;
+    }
+    }
 
-        // string 1
+    function getKeyScaleNotes(key, scale){
+        var keyNotes = getKeyNotes(key);
+        var scalePosition = getScalePosition(scale);
+        var keyScaleNotes = new Array();
+        for(var i=0;i<keyNotes.length;i=i+1){
+           if($.inArray(i, scalePosition) !==-1){
+                keyScaleNotes.push(keyNotes[i]);
+            }
+        }
+        return keyScaleNotes;
+    }
+
+    function changeFingerBoard(key,scale){
+
+        var keyScaleNotes = getKeyScaleNotes(key,scale);
+
         $(".string_1 .fret").each(function(index,element){
-            var n = noteMap[(tuneIndex[5]+index+1)];
-            if($.inArray( n, note ) !== -1){
-                $(this).text(n);
+            var note = twelveToneEqualTemperament[(tunePosition[5]+index+1)%twelveToneEqualTemperament.length];
+            if($.inArray( note, keyScaleNotes ) !== -1){
+                $(this).text(note);
             }else{
                 $(this).text("");
             }
         });
         $(".string_2 .fret").each(function(index,element){
-            var n = noteMap[(tuneIndex[4]+index+1)];
-            if($.inArray( n, note ) !== -1){
-                $(this).text(n);
+            var note = twelveToneEqualTemperament[(tunePosition[4]+index+1)%twelveToneEqualTemperament.length];
+            if($.inArray( note, keyScaleNotes ) !== -1){
+                $(this).text(note);
             }else{
                 $(this).text("");
             }
         });
         $(".string_3 .fret").each(function(index,element){
-            var n = noteMap[(tuneIndex[3]+index+1)];
-            if($.inArray( n, note ) !== -1){
-                $(this).text(n);
+            var note = twelveToneEqualTemperament[(tunePosition[3]+index+1)%twelveToneEqualTemperament.length];
+            if($.inArray( note, keyScaleNotes ) !== -1){
+                $(this).text(note);
             }else{
                 $(this).text("");
             }
         });
         $(".string_4 .fret").each(function(index,element){
-            var n = noteMap[(tuneIndex[2]+index+1)];
-            if($.inArray( n, note ) !== -1){
-                $(this).text(n);
+            var note = twelveToneEqualTemperament[(tunePosition[2]+index+1)%twelveToneEqualTemperament.length];
+            if($.inArray( note, keyScaleNotes ) !== -1){
+                $(this).text(note);
             }else{
                 $(this).text("");
             }
         });
         $(".string_5 .fret").each(function(index,element){
-            var n = noteMap[(tuneIndex[1]+index+1)];
-            if($.inArray( n, note ) !== -1){
-                $(this).text(n);
+            var note = twelveToneEqualTemperament[(tunePosition[1]+index+1)%twelveToneEqualTemperament.length];
+            if($.inArray( note, keyScaleNotes ) !== -1){
+                $(this).text(note);
             }else{
                 $(this).text("");
             }
         });
         $(".string_6 .fret").each(function(index,element){
-            var n = noteMap[(tuneIndex[0]+index+1)];
-            if($.inArray( n, note ) !== -1){
-                $(this).text(n);
+            var note = twelveToneEqualTemperament[(tunePosition[0]+index+1)%twelveToneEqualTemperament.length];
+            if($.inArray( note, keyScaleNotes ) !== -1){
+                $(this).text(note);
             }else{
                 $(this).text("");
             }
@@ -139,4 +117,3 @@ $( document ).ready(function() {
 
 
 });
-
